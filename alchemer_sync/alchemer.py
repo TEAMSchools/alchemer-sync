05506a7529972162613a312d0b1cfdac6359cbf7
Charlie Bini <5003326+cbini@users.noreply.github.com>
2021-10-07 18:13:41 -0500

    expand classes

--- alchemer_sync/alchemer.py
+++ alchemer_sync/alchemer.py
@@ -1,7 +1,5 @@
 import requests
 
-# DEFAULT_RESULTS_PER_PAGE = 50
-
 
 class AlchemerSession(requests.Session):
     def __init__(self, api_version, api_token, api_token_secret, auth_method="api_key"):
@@ -26,7 +24,7 @@ class AlchemerSession(requests.Session):
         super(AlchemerSession, self).__init__()
 
     def request(self, method, url, *args, **kwargs):
-        id = kwargs.pop("id")
+        id = kwargs.pop("id", "")
         url = f"{self.base_url}/{url}/{id}"
         self.params.update(self.base_params)
         return super(AlchemerSession, self).request(
@@ -60,16 +58,61 @@ class AlchemerSession(requests.Session):
 
         return all_data
 
-    def survey(self, id):
-        return Survey(self, id)
+    def router(self, name, id):
+        if id:
+            if name == "survey":
+                return Survey(self, id)
+            elif name == "account":
+                return Account(self, id)
+        else:
+            return self.get_object_data(object_name=name)
+
+    def survey(self, id=None):
+        return self.router("survey", id)
+
+    def account(self, id=None):
+        return self.router("account", id)
 
 
-class Survey(object):
-    def __init__(self, session, id):
-        self.session = session
+class AlchemerObject(object):
+    def __init__(self, session, name, id):
+        self.__name__ = name
+        self._session = session
         self.id = id
+        self._session.base_url = f"{self._session.base_url}/{self.__name__}/{self.id}"
 
-        self.data = self.session.get_object_data(object_name="survey", id=self.id)[0]
+        data = next(
+            iter(self._session.get_object_data(object_name=self.__name__, id=id)),
+            {},
+        )
+        for k, v in data.items():
+            setattr(self, k, v)
 
-    def question(self):
-        pass
+    def _subobject(self, name, id):
+        if id:
+            return AlchemerObject(self._session, name, id)
+        else:
+            return self._session.get_object_data(object_name=name)
+
+
+class Survey(AlchemerObject):
+    def __init__(self, session, id):
+        super().__init__(session, "survey", id)
+
+    def question(self, id=None):
+        return self._subobject("surveyquestion", id)
+
+    def campaign(self, id=None):
+        return self._subobject("surveycampaign", id)
+
+
+class SurveyQuestion(AlchemerObject):
+    pass
+
+
+class Account(AlchemerObject):
+    def __init__(self, session, id):
+        super().__init__(session, "account", id)
+
+    def teams(self, id=None):
+        return super()._subobject("accountteams", id)
